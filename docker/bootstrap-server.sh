8ee3b66f1747bc840c43310ad8d3b0c39f7d3452
Edmundo Alvarez <edmundo@graylog.com>
2016-11-08 16:48:47 +0100

    Update url to check to ensure Graylog runs

--- docker/bootstrap-server.sh
+++ docker/bootstrap-server.sh
@@ -4,4 +4,4 @@ PREFIX=`dirname $0`
 docker-compose -f $PREFIX/docker-compose.yml up &
 
 echo -n "Waiting for Graylog to come up"
-until `curl -s http://localhost:12900/ > /dev/null`; do echo -n "."; sleep 1; done
+until `curl -s http://localhost:9000/api > /dev/null`; do echo -n "."; sleep 1; done
