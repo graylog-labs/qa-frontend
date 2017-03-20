dc2bda3dc6d3d3035d9207031ddf7a47ae60d83f
Dennis Oelkers <dennis@graylog.com>
2017-03-20 11:52:38 +0100

    Running session registration before test group.

--- ruby/spec/features/streams_spec.rb
+++ ruby/spec/features/streams_spec.rb
@@ -5,10 +5,12 @@ include SessionHelpers
 describe "Creating a stream", :type => :feature do
 
   before(:all) do
-    @session = register_session(admin_credentials[:user], admin_credentials[:password])
     @streamName = randomStreamName
+    @session = register_session(admin_credentials[:user], admin_credentials[:password])
+    visit '/'
+    login_with_valid_session(admin_credentials[:user], @session["session_id"])
   end
-  
+
   def streamItemSelector(streamName)
     "//li[@class=\"stream\" and descendant::node()[text()=\"" + streamName + "\"]]"
   end
@@ -16,14 +18,12 @@ describe "Creating a stream", :type => :feature do
   def randomStreamName
     ('a'..'z').to_a.shuffle[0,8].join
   end
-  
+
   it "should create stream" do
-    visit '/'
-    login_with_valid_session(admin_credentials[:user], @session["session_id"])
     visit '/'
     find_link("Streams").click
     click_button("Create Stream")
-    
+
     find(:xpath, "//input[@label='Title']").set @streamName
     find(:xpath, "//input[@label='Description']").set "Testing"
     find(:xpath, "//button[.='Save']").click
@@ -31,7 +31,7 @@ describe "Creating a stream", :type => :feature do
     expect(page).to have_selector(:xpath, "//li[@class=\"stream\" and descendant::node()[text()=\"" + @streamName + "\"]]/descendant::node()[text()=\"Stopped\"]")
     expect(page).to have_selector(:xpath, "//li[@class=\"stream\" and descendant::node()[text()=\"" + @streamName + "\"]]/descendant::node()[@class=\"stream-description\"]/descendant::node()[text()=\"Testing\"]")
   end
-  
+
   it "should add stream rule" do
     visit "/streams"
     selector = streamItemSelector(@streamName)
@@ -39,12 +39,12 @@ describe "Creating a stream", :type => :feature do
     within(:xpath, selector) do
       click_link("Manage Rules")
     end
-    
+
     click_link_or_button("Add stream rule")
     find(:xpath, "//input[@label='Field' and contains(@class, 'tt-input')]").set "message"
     find(:xpath, "//input[@label='Value']").set "foo"
     click_link_or_button("Save")
-    
+
     expect(page).to have_selector(:xpath, "//ul[@class=\"streamrules-list\"]/li[descendant::node()[text()=\"foo\"]]")
   end
 
