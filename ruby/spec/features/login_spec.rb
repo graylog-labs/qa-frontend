2a68212ec87134e7321ae3ff90206dab961988c7
Dennis Oelkers <dennis@graylog.com>
2017-03-20 12:59:01 +0100

    Using within to scope find.

--- ruby/spec/features/login_spec.rb
+++ ruby/spec/features/login_spec.rb
@@ -4,23 +4,23 @@ describe "Login", :type => :feature do
 
   it "should reject invalid logins" do
     visit "/"
-    find('#login-box-content') { |loginbox|
-      loginbox.find(:xpath, "//input[@placeholder='Username']").set("admin")
-      loginbox.find(:xpath, "//input[@placeholder='Password']").set("invalid")
-      loginbox.find(:xpath, "//input[@value='Sign in']").click
+    within('div#login-box') {
+      find(:xpath, "//input[@placeholder='Username']").set("admin")
+      find(:xpath, "//input[@placeholder='Password']").set("invalid")
+      find(:xpath, "//input[@value='Sign in']").click
     }
-    
+
     expect(find(".alert-danger")).to have_content("Invalid credentials, please verify them and retry.")
   end
-  
+
   it "should allow admin login with correct password" do
     visit "/"
-    find('#login-box-content') { |loginbox|
-      loginbox.find(:xpath, "//input[@placeholder='Username']").set(admin_credentials[:user])
-      loginbox.find(:xpath, "//input[@placeholder='Password']").set(admin_credentials[:password])
-      loginbox.find(:xpath, "//input[@value='Sign in']").click
+    within('div#login-box') {
+      find(:xpath, "//input[@placeholder='Username']").set(admin_credentials[:user])
+      find(:xpath, "//input[@placeholder='Password']").set(admin_credentials[:password])
+      find(:xpath, "//input[@value='Sign in']").click
     }
-    
+
     expect(page).to have_selector(:xpath, "//a[@id='user-menu-dropdown']/descendant::span[.='Administrator']")
   end
 end
