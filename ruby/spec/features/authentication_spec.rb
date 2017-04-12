881214c6bec28979f574dc40d19e35b65fab3ceb
Florian Volle <florian@graylog.com>
2017-04-12 17:06:48 +0200

    add authentication test

--- ruby/spec/features/authentication_spec.rb
+++ ruby/spec/features/authentication_spec.rb
@@ -1,11 +1,12 @@
 include RSpec::Expectations
-include SessionHelpers
 include GenericHelpers
+include SessionHelpers
 
 describe "Creating a user", :type => :feature do
 
   before(:all) do
     @adminName = randomName
+    @userName = randomName
     @session = register_session(admin_credentials[:user], admin_credentials[:password])
     visit '/'
     login_with_valid_session(admin_credentials[:user], @session["session_id"])
@@ -28,6 +29,7 @@ describe "Creating a user", :type => :feature do
     click_button("Create User")
     expect(page).to have_link(@adminName)
   end
+
   it "should delete admin user" do
     visit '/'
     find_link("System").click
@@ -39,6 +41,34 @@ describe "Creating a user", :type => :feature do
     end
 
     expect(page).not_to have_link(@adminName)
+  end
 
+  it "should create readonly user" do
+    visit '/'
+    find_link("System").click
+    find_link("Authentication").click
+    click_link_or_button("Add new user")
+
+    find(:id, "username").set @userName
+    find(:id, "fullname").set "John Reader"
+    find(:id, "email").set "test@example.org"
+    find(:id, "password").set "123456"
+    find(:id, "password-repeat").set "123456"
+    within(:xpath, '//div[@class="form-group" and descendant::node()[text()="Roles"]]') do
+      find(:css, "div.Select-control input").set "Reader"
+    end
+    click_button("Create User")
+    expect(page).to have_link(@userName)
+  end
+
+  it "should delete readonly user" do
+    visit '/'
+    find_link("System").click
+    find_link("Authentication").click
+    accept_alert "Do you really want to delete user #{@userName}?" do
+      within(:id,"user-list") do
+        find(:id, "delete-user-#{@userName}" ).click
+      end
+    end
   end
 end
