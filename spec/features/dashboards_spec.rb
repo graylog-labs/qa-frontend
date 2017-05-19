94a0347a140e2b6fa97911d1f01d1deb73834717
Florian Volle <florian@graylog.com>
2017-05-19 17:15:42 +0200

    add edit dashboard

--- spec/features/dashboards_spec.rb
+++ spec/features/dashboards_spec.rb
@@ -7,35 +7,42 @@ describe "Dashboards", :type => :feature do
 
   before(:all) do
     @dashboardName = randomName
+    @newdashboardName = randomName
   end
 
   it "should create new dashboard" do
     visit "/"
     click_on("Dashboards")
-
     expect(page).not_to have_link(@dashboardName)
-
     click_button("Create dashboard")
-
     fill_in("Title", with: @dashboardName)
     fill_in("Description", with: "Testing")
     click_button("Save")
-
     expect(page).to have_link(@dashboardName)
+  end
+
+  it "should edit dashboard" do
+    visit "/Dashboards"
+    click_on("Edit dashboard").set @dashboardName
+    fill_in("Title", with: @newdashboardName)
+    fill_in("Description", with: "Edited")
+    click_on("Save")
+    expect(page).to have_link(@newdashboardName)
+
   end
 
   it "should delete new dashboard" do
     visit "/dashboards"
 
-    within(find_dashboard(@dashboardName)) do
+    within(find_dashboard(@newdashboardName)) do
       click_on "More actions"
 
-      accept_alert "Do you really want to delete the dashboard #{@dashboardName}" do
+      accept_alert "Do you really want to delete the dashboard #{@newdashboardName}" do
         click_on("Delete this dashboard")
       end
     end
 
-    expect(page).not_to have_link(@dashboardName)
+    expect(page).not_to have_link(@newdashboardName)
   end
 
   def find_dashboard(dashboardName)
