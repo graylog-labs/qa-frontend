79be641e6e09ac3800950e32382f22dc078f29bb
Dennis Oelkers <dennis@graylog.com>
2017-05-22 15:12:36 +0200

    Extracting helpers, making matchers more general

--- spec/features/streams_spec.rb
+++ spec/features/streams_spec.rb
@@ -15,62 +15,72 @@ describe "Creating a stream", :type => :feature do
     expect(current_path).to eq("/streams")
     click_button("Create Stream")
 
-    fill_in("Title", with: @streamName)
-    fill_in("Description", with: "Testing")
-    click_on("Save")
-    expect(page).to match have_link(@streamName, wait: 30)
+    within(modal_dialog) do
+      fill_in(name: "Title", with: @streamName)
+      fill_in(name: "Description", with: "Testing")
+      click_on("Save")
+    end
+    expect(page).to match have_link(@streamName)
     expect(page).to have_text("Stopped")
     expect(page).to have_content("Testing")
   end
 
   it "should add stream rule" do
     visit "/streams"
-    within(find_stream(@streamName, wait: 30)) do
+    within(find_stream(@streamName)) do
       click_on("Manage Rules")
     end
     click_on("Add stream rule")
 
-    within(find(:id, "StreamRuleForm")) do
-      fill_in("Field", with: "message", class: "tt-input")
-      fill_in("Value", with: "foo")
+    within(modal_dialog) do
+      fill_in("Field", class: "tt-input", with: "message")
+      fill_in(name: "Value", with: "foo")
       click_on("Save")
     end
 
-    expect(find("ul", class: "streamrules-list")).to have_text("Field message must match exactly foo")
+    expect(streamrules_list).to have_text("Field message must match exactly foo")
   end
 
   it "should start the stream" do
     visit "/streams"
-    within(find_stream(@streamName, wait: 30)) do
+    within(find_stream(@streamName)) do
       click_on "Start Stream"
     end
 
-    expect(find_stream(@streamName)).to have_no_content("Stopped", wait: 30)
+    expect(find_stream(@streamName)).to have_no_content("Stopped")
   end
 
   it "should pause the stream" do
     visit "/streams"
-    within(find_stream(@streamName, wait: 30)) do
+    within(find_stream(@streamName)) do
       accept_alert("Do you really want to pause stream '#{@streamName}'?") do
         click_on "Pause Stream"
       end
     end
 
-    expect(find_stream(@streamName)).to have_content("Stopped", wait: 30)
+    expect(find_stream(@streamName)).to have_content("Stopped")
   end
 
   it "should delete the stream" do
     visit "/streams"
-    within(find_stream(@streamName, wait: 30)) do
+    within(find_stream(@streamName)) do
       click_button("More Actions")
       accept_alert "Do you really want to remove this stream" do
         click_link("Delete this stream")
       end
     end
-    expect(page).to have_no_link(@streamName, wait: 30)
+    expect(page).to have_no_link(@streamName)
   end
 
   def find_stream(streamName, options = {})
     find(:xpath, "//li[@class=\"stream\" and descendant::node()[text()=\"" + streamName + "\"]]", options)
+  end
+
+  def modal_dialog
+    find("div", class: "modal")
+  end
+
+  def streamrules_list
+    find("ul", class: "streamrules-list")
   end
 end
