c0fb3399cc6fa02646faf3d1b4e7de884d488e8d
Dennis Oelkers <dennis@graylog.com>
2017-05-22 15:14:43 +0200

    Adding helpers for typeahead dropdowns.

--- spec/helpers/generic_helpers.rb
+++ spec/helpers/generic_helpers.rb
@@ -2,4 +2,20 @@ module GenericHelpers
   def randomName
     ('a'..'z').to_a.shuffle[0,8].join
   end
+
+  def fill_typeahead(title, options)
+    value = options.delete(:with)
+    select = find("label", text: title)
+      .first(:xpath, ".//..")
+      .find("div", { class: "Select" }.merge(options))
+      .click
+    select.find("div", text: /^\s*#{value}\s*$/).click
+  end
+
+  def clear_typeahead(title, options = {})
+    find("label", text: title)
+      .first(:xpath, ".//..")
+      .find("span", class: "Select-clear")
+      .click
+  end
 end
