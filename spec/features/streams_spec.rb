fbf2480d2314c3922b0d09bd3e85ab8837a9cd6e
Dennis Oelkers <dennis@graylog.com>
2017-05-23 14:06:30 +0200

    Adding more test cases for stream rules.

--- spec/features/streams_spec.rb
+++ spec/features/streams_spec.rb
@@ -2,7 +2,7 @@ include RSpec::Expectations
 include SessionHelpers
 include GenericHelpers
 
-describe "Creating a stream", :type => :feature do
+describe "Stream functionality", :type => :feature do
   include_examples "authenticated"
 
   before(:all) do
@@ -13,7 +13,7 @@ describe "Creating a stream", :type => :feature do
     visit "/streams"
   end
 
-  it "should create stream" do
+  it "creates new stream" do
     click_button("Create Stream")
 
     within(modal_dialog) do
@@ -26,7 +26,7 @@ describe "Creating a stream", :type => :feature do
     expect(page).to have_content("Testing")
   end
 
-  it "should add stream rule" do
+  it "adds stream rule" do
     within(find_stream(@streamName)) do
       click_on("Manage Rules")
     end
@@ -38,12 +38,17 @@ describe "Creating a stream", :type => :feature do
       click_on("Save")
     end
 
+    expect(streamrules_list).to have_text("Field message must match exactly foo")
+
+    click_on "I'm done!"
+
     within(find_stream(@streamName)) do
+      click_on "Show stream rules"
       expect(streamrules_list).to have_text("Field message must match exactly foo")
     end
   end
 
-  it "should start the stream" do
+  it "starts a stopped stream" do
     within(find_stream(@streamName)) do
       click_on "Start Stream"
     end
@@ -51,7 +56,7 @@ describe "Creating a stream", :type => :feature do
     expect(find_stream(@streamName)).to have_no_content("Stopped")
   end
 
-  it "should pause the stream" do
+  it "pauses a running stream" do
     within(find_stream(@streamName)) do
       accept_alert("Do you really want to pause stream '#{@streamName}'?") do
         click_on "Pause Stream"
@@ -61,7 +66,48 @@ describe "Creating a stream", :type => :feature do
     expect(find_stream(@streamName)).to have_content("Stopped")
   end
 
-  it "should delete the stream" do
+  it "changes the field name of a stream rule using streamrules list in overview" do
+    within(find_stream(@streamName)) do
+      click_on "Show stream rules"
+      within(streamrules_list) do
+        within(find_streamrule("Field message must match exactly foo")) do
+          find("i", class: "fa-edit").click
+        end
+      end
+    end
+
+    within(modal_dialog) do
+      fill_in("Field", class: "tt-input", with: "source")
+
+      click_on "Save"
+    end
+
+    within(find_stream(@streamName)) do
+      click_on "Show stream rules"
+    end
+
+    expect(find_stream(@streamName)).not_to have_text("Field message must match exactly foo")
+    expect(find_stream(@streamName)).to have_text("Field source must match exactly foo")
+  end
+
+  it "deletes a stream rule using streamrules list in overview" do
+    within(find_stream(@streamName)) do
+      click_on "Show stream rules"
+      within(streamrules_list) do
+        within(find_streamrule("Field source must match exactly foo")) do
+          accept_alert "Do you really want to delete this stream rule?" do
+            find("i", class: "fa-trash-o").click
+          end
+        end
+      end
+
+      click_on "Show stream rules"
+      expect(streamrules_list).not_to have_text("Field message must match exactly foo")
+      expect(streamrules_list).to have_text("No rules defined.")
+    end
+  end
+
+  it "deletes the stream" do
     within(find_stream(@streamName)) do
       click_button("More Actions")
       accept_alert "Do you really want to remove this stream" do
@@ -81,5 +127,9 @@ describe "Creating a stream", :type => :feature do
 
   def streamrules_list
     find("ul", class: "streamrules-list")
+  end
+
+  def find_streamrule(streamRule)
+    find("li", text: streamRule)
   end
 end
